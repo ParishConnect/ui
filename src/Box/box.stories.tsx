2ac5e4d5e28ff44062bc275258ec79e705331942
hennessyevan <hennessyevan@users.noreply.github.com>
2019-11-06 17:02:50 -0500

    edit box story

--- src/Box/box.stories.tsx
+++ src/Box/box.stories.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Theme } from '../theme'
 import { Box } from './Box'
 
 export default {
-  title: 'Box',
-  component: Box,
+	title: 'Box',
+	component: Box,
 }
 
 export const box = () => {
-  return (
-    <Box tint borderRadius={2} padding={1} width={200} height={200}>
-      Hi this is a Box component!
-    </Box>
-  )
+	return (
+		<Box display="grid" gridTemplateColumns="repeat(auto-fit, 200px)" gridGap={3}>
+			{Object.keys(new Theme().colors).map(color => (
+				<Box width={200} height={200} tint={color}>
+					{color}
+				</Box>
+			))}
+		</Box>
+	)
 }
