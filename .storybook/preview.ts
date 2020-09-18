8c56a2f27ae011551f3466f5e75d79bc85209b1c
hennessyevan <hennessyevan@users.noreply.github.com>
2020-09-18 12:50:52 -0400

    F add theme color to preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -14,7 +14,7 @@ addDecorator(
           props: { colorMode: 'light' },
           default: true,
         },
-        ...['purple', 'orange', 'teal', 'green'].map(color => ({
+        ...['red', 'purple', 'orange', 'teal', 'green'].map(color => ({
           name: `Light ${color}`,
           props: { colorMode: 'light', color },
         })),
@@ -22,7 +22,7 @@ addDecorator(
           name: 'Dark',
           props: { colorMode: 'dark' },
         },
-        ...['purple', 'orange', 'teal', 'green'].map(color => ({
+        ...['red', 'purple', 'orange', 'teal', 'green'].map(color => ({
           name: `Dark ${color}`,
           props: { colorMode: 'dark', color },
         })),
